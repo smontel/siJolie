97172c316f93b529adc692ce952638663518aa6b
smontel <montelsylvain@gmail.com>
2017-05-29 17:02:19 +0200

    bouton descente / montée

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,26 @@
-$(".bdown").click(function(){
-  $("body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 1000);
-  return false;
+$("#butt").click(function(){
+  if($('#butt').hasClass("bdown")){
+    $("body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 1000);
+    return false;
+  }
+  else if ($('#butt').hasClass("bup")) {
+    console.log('ca devrait monter');
+    $("body").animate({ scrollTop: 0}, 1000);
+    return false;
+  }
+});
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       $('#butt').removeClass('bdown');
+       $("#butt").addClass('bup');
+
+       return false;
+   }
+   else{
+     $('#butt').removeClass('bup');
+     $("#butt").addClass('bdown');
+
+     return false;
+   }
 });
